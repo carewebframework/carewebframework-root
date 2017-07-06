7e212927f49f100a17a537d3813dcb31f6e48d43
Douglas Martin <dkmartin@regenstrief.org>
2017-07-06 17:12:39 -0400

    Set up code signing.

--- gpg/before-install.sh
+++ gpg/before-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  cd cd
+  cd gpg
   openssl aes-256-cbc -K $encrypted_602af71d785c_key -iv $encrypted_602af71d785c_iv -in codesigning.asc.enc -out codesigning.asc -d
   gpg --fast-import codesigning.asc
 fi
