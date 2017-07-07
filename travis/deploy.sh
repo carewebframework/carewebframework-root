b54b34085baef1ae1b7ebc85c8093fcf636a710c
Douglas Martin <dkmartin@regenstrief.org>
2017-07-07 09:35:14 -0400

    Travis updates.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_602af71d785c_key -iv $encrypted_602af71d785c_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+  openssl aes-256-cbc -K $encrypted_c7ab747c4764_key -iv $encrypted_c7ab747c4764_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
   gpg --fast-import travis/codesigning.asc
   mvn -V -B -s travis/settings.xml clean deploy -P sign
 fi
